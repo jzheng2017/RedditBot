b2e85d033b4c1a2e36d9c4b3ef312ea300372ccf
jzheng2017 <jianjian-r@hotmail.com>
2017-03-14 12:48:18 +0100

    Reddit bot
    Added reddit bot python file.
    - config.py (it contains the configuration data)
    - reddbot.py (the bot itself)

---
+++ RedditBot/src/redditbot.py
@@ -0,0 +1,9 @@
+import praw
+import config
+
+
+praw.Reddit(username = config.username,
+			password = config.password,
+			client_id = config.client_id,
+			client_secret = config.client_secret,
+			user_agent = 'Reddit Search Bot v1.0');
