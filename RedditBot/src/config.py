0174e608d0b861e9ae9e2376fa2c77db652c6854
jzheng2017 <jianjian-r@hotmail.com>
2017-03-15 21:11:04 +0100

    Some changes
    -config extension to .py
    -info changed in config.py
    -methods added in redditbot.py

---
+++ RedditBot/src/config.py
@@ -0,0 +1,4 @@
+username = "RedditBot2017"
+password = "asdasd34"
+client_id = "OIxXtk0Vik4OWg"
+client_secret = "f_zIMKLV9Apr-z6iCL8d-h2Mpow"
--- RedditBot/src/redditbot.py
+++ RedditBot/src/redditbot.py
@@ -1,9 +1,22 @@
 import praw
 import config
 
-
-praw.Reddit(username = config.username,
+def bot_login():
+	r = praw.Reddit(username = config.username,
 			password = config.password,
 			client_id = config.client_id,
 			client_secret = config.client_secret,
-			user_agent = 'Reddit Search Bot v1.0');
+			user_agent = 'Reddit Search Bot v1.0')
+	return r
+
+
+def run_bot(r):
+	for comment in r.subreddit('test').comment(limit=25):
+		if "test" in comment.body:
+			print("String found")
+			comment.reply("test found")
+
+
+r = bot_login()
+
+run_bot(r)
