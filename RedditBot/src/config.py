e87436d6b55a8ecedde22221238880a659c24792
jzheng2017 <jianjian-r@hotmail.com>
2017-04-06 08:45:45 +0200

    config changes
    some changes to config.py

--- RedditBot/src/config.py
+++ RedditBot/src/config.py
@@ -1,4 +1,4 @@
-username = "RedditBot2017"
-password = "asdasd34"
-client_id = "OIxXtk0Vik4OWg"
-client_secret = "f_zIMKLV9Apr-z6iCL8d-h2Mpow"
+username = "YourUsername"
+password = "YourPassword"
+client_id = "client_id"
+client_secret = "client_secret"
